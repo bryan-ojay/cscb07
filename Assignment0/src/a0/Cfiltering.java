69e3efcceb7eb49834aa76ba2c851e6da6cd10ba
Bryan Oladeji <boladeji@hotmail.com>
2019-04-12 22:49:46 -0400

    A0 commit

--- Assignment0/src/a0/Cfiltering.java
+++ Assignment0/src/a0/Cfiltering.java
@@ -1,9 +1,4 @@
 // **********************************************************
-// Assignment0:
-// UTORID: oladejib
-// UT Student #: 1004112738
-// Author: Bryan Oladeji
-//
 // Honor Code: I pledge that this program represents my own
 // program code and that I have coded on my own. I received
 // help from no one in designing and debugging my program.
